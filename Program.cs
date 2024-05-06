46dceee19e1e28571d32861c22ff3baf2e00a9ba
jianrong7 <ipwnjianrong@gmail.com>
2024-05-06 20:05:32 +0800

    implement opt '

--- Program.cs
+++ Program.cs
@@ -22,116 +22,95 @@ app.UseHttpsRedirection();
 /*
 input: list of frame numbers, number of frames
 output: [number of frames * list of each time], total number of page faults
-{
-  "incomingFrames": [2,3,2,1,5,2,4,5,3,2,5,2],
-  "numberOfFrames": 3
-}
 */
 
-app.MapPost(
-    "/opt",
-    (List<int> incomingFrames, int numberOfFrames) =>
-    {
-        var indexedIncomingFrames = incomingFrames
-            .Select((frame, index) => (frame, index))
-            .ToList();
-        var frames = new Dictionary<int, int>();
-        var framesNextUseTime = new Dictionary<int, int>();
-        var result = new List<(int, int)>();
-        var pageFaults = 0;
-
-        for (int i = 0; i < numberOfFrames; i++)
-        {
-            // (frame, next use time). (-1, -1) means frame is empty and that next use time is infinity
-            frames.Add(i, -1);
-            framesNextUseTime.Add(i, -1);
-        }
-
-        for (int i = 0; i < incomingFrames.Count; i++)
-        {
-            var f = incomingFrames[i];
-            // already in frames, no page fault
-            if (frames.ContainsValue(f))
-            {
-                var key = frames.FirstOrDefault(x => x.Value == f).Key;
-                for (int j = i + 1; j < incomingFrames.Count; j++)
-                {
-                    if (incomingFrames[j] == f)
-                    {
-                        framesNextUseTime[key] = j;
-                        break;
-                    }
-                }
-                continue;
-            }
-            else
-            { // not in frames, page fault
-                pageFaults++;
-                bool inserted = false;
-                var maxUseTime = 0;
-                var maxUseTimeFrame = 0;
-                for (int j = 0; j < numberOfFrames; j++)
-                {
-                    if (framesNextUseTime[j] > maxUseTime)
-                    {
-                        maxUseTime = framesNextUseTime[j];
-                        maxUseTimeFrame = j;
-                    }
-                    if (frames[j] == -1)
-                    {
-                        frames[j] = f;
-                        framesNextUseTime[j] = indexedIncomingFrames
-                            .FirstOrDefault(x => x.frame == f)
-                            .index;
-                        inserted = true;
-                        break;
-                    }
-                }
-                if (!inserted)
-                {
-                    frames[maxUseTimeFrame] = f;
-                    framesNextUseTime[maxUseTimeFrame] = indexedIncomingFrames
-                        .FirstOrDefault(x => x.frame == f)
-                        .index;
-                }
-                // var kv = frames.FirstOrDefault(x => x.Value == -1);
-                // if (kv.Key != -1 || kv.Value != -1) // all filled up already
-                // {
-                //     var maxNextUseTime = framesNextUseTime.Max(x => x.Value);
-                //     var frameToReplace = framesNextUseTime
-                //         .FirstOrDefault(x => x.Value == maxNextUseTime)
-                //         .Key;
-                //     frames[frameToReplace] = f;
-                //     framesNextUseTime[frameToReplace] = indexedIncomingFrames
-                //         .FirstOrDefault(x => x.frame == f)
-                //         .index;
-                // }
-                // else // not filled up yet
-                // {
-                //     frames[kv.Key] = f;
-                //     framesNextUseTime[kv.Key] = indexedIncomingFrames
-                //         .FirstOrDefault(x => x.frame == f)
-                //         .index;
-                // }
-            }
-
-            for (int k = 0; k < numberOfFrames; k++)
-            {
-                Console.WriteLine("=========");
-                Console.WriteLine($"Frame {k}: {frames[k]}, Next use time: {framesNextUseTime[k]}");
-            }
-        }
-
-        return "Hello world";
-    }
-);
-
+app.MapPost("/opt", Opt);
 app.MapPost("/fifo", Fifo);
 app.MapPost("/lru", Lru);
 app.MapPost("/clock", Clock);
 
-// OPT, LRU, CLOCK
 app.Run();
+
+static Result Opt(Input input)
+{
+    var frames = new Dictionary<int, int>();
+
+    var pageFaults = new List<bool>();
+    var pageFaultsCount = 0;
+    var result = new Dictionary<int, List<int>>();
+
+    for (int i = 0; i < input.NumberOfFrames; i++)
+    {
+        // (frame, next use time). (-1, -1) means frame is empty and that next use time is infinity
+        frames.Add(i, -1);
+        result.Add(i, new List<int>());
+    }
+
+    for (int i = 0; i < input.IncomingFrames.Count; i++)
+    {
+        var f = input.IncomingFrames[i];
+        // check if values of frames contain f
+        if (frames.ContainsValue(f))
+        {
+            // if contains, do nothing
+            pageFaults.Add(false);
+            // continue;
+        }
+        else
+        {
+            pageFaults.Add(true);
+            pageFaultsCount++;
+            // if not, check if there is an empty frame
+            var inserted = false;
+            for (int j = 0; j < input.NumberOfFrames; j++)
+            {
+                if (frames[j] == -1)
+                {
+                    // if there is, insert frame into empty frame
+                    frames[j] = f;
+                    inserted = true;
+                    break;
+                }
+            }
+            // if not, find the frame that has the largest next use time and replace that frame with f
+            if (!inserted)
+            {
+                var frameWithLargestNextUseTime = -1;
+                var largestNextUseTime = 0;
+                for (int k = 0; k < input.NumberOfFrames; k++)
+                {
+                    var neverUsed = true;
+                    for (int j = i + 1; j < input.IncomingFrames.Count; j++)
+                    {
+                        if (frames[k] == input.IncomingFrames[j])
+                        {
+                            neverUsed = false;
+                            if (j > largestNextUseTime)
+                            {
+                                largestNextUseTime = j;
+                                frameWithLargestNextUseTime = k;
+                            }
+                            break;
+                        }
+                    }
+                    if (neverUsed)
+                    {
+                        frameWithLargestNextUseTime = k;
+                        break;
+                    }
+                }
+                frames[frameWithLargestNextUseTime] = f;
+            }
+        }
+
+        for (int j = 0; j < input.NumberOfFrames; j++)
+        {
+            result[j].Add(frames[j]);
+        }
+    }
+
+    return new Result(pageFaults, pageFaultsCount, result);
+}
 
 static Result Fifo(Input input)
 {
